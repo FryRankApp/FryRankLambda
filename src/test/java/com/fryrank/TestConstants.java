71e4a6ede5de63dda1f5bf15325d31cf5f4cd915
Nicholas <nicholaspriv@gmail.com>
2025-03-12 22:00:30 -0700

    Adding back in some needed dependencies

--- src/test/java/com/fryrank/TestConstants.java
+++ src/test/java/com/fryrank/TestConstants.java
@@ -1,6 +1,8 @@
 package com.fryrank;
 
 import com.fryrank.model.Review;
+import com.fryrank.model.PublicUserMetadata;
+import com.fryrank.model.PublicUserMetadataOutput;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,82 +24,71 @@ public class TestConstants {
     public static final String TEST_USERNAME = "testflush";
 
     public static final PublicUserMetadata TEST_USER_METADATA_1 = new PublicUserMetadata(
-        TEST_ACCOUNT_ID,
-        TEST_USERNAME
+            TEST_ACCOUNT_ID,
+            TEST_USERNAME
     );
 
     public static final PublicUserMetadataOutput TEST_USER_METADATA_OUTPUT_1 = new PublicUserMetadataOutput(
-        TEST_USERNAME
+            TEST_USERNAME
     );
 
     public static final Review TEST_REVIEW_1 = new Review(
-        TEST_REVIEW_ID_1,
-        TEST_RESTAURANT_ID,
-        5.0 ,
-        TEST_TITLE_1,
-        TEST_BODY_1,
-        TEST_ISO_DATE_TIME_1,
-        TEST_ACCOUNT_ID,
-        null
+            TEST_REVIEW_ID_1,
+            TEST_RESTAURANT_ID,
+            5.0 ,
+            TEST_TITLE_1,
+            TEST_BODY_1,
+            TEST_ISO_DATE_TIME_1,
+            TEST_ACCOUNT_ID,
+            null
     );
 
     public static final Review TEST_REVIEW_NULL_ISO_DATETIME = new Review(
-        TEST_REVIEW_ID_1,
-        TEST_RESTAURANT_ID_1,
-        5.0,
-        TEST_TITLE_1,
-        TEST_BODY_1,
-        null,
-        TEST_ACCOUNT_ID,
-        TEST_USER_METADATA_1
+            TEST_REVIEW_ID_1,
+            TEST_RESTAURANT_ID_1,
+            5.0,
+            TEST_TITLE_1,
+            TEST_BODY_1,
+            null,
+            TEST_ACCOUNT_ID,
+            TEST_USER_METADATA_1
     );
 
     public static final Review TEST_REVIEW_BAD_ISO_DATETIME = new Review(
-        TEST_REVIEW_ID_1,
-        TEST_RESTAURANT_ID_1,
-        5.0,
-        TEST_TITLE_1,
-        TEST_BODY_1,
-        "not-a-real-date",
-        TEST_ACCOUNT_ID,
-        TEST_USER_METADATA_1
+            TEST_REVIEW_ID_1,
+            TEST_RESTAURANT_ID_1,
+            5.0,
+            TEST_TITLE_1,
+            TEST_BODY_1,
+            "not-a-real-date",
+            TEST_ACCOUNT_ID,
+            TEST_USER_METADATA_1
     );
 
     public static final Review TEST_REVIEW_NULL_ACCOUNT_ID = new Review(
-        TEST_REVIEW_ID_1,
-        TEST_RESTAURANT_ID,
-        5.0 ,
-        TEST_TITLE_1,
-        TEST_BODY_1,
-        TEST_ISO_DATE_TIME_1,
-        null,
-        TEST_USER_METADATA_1
+            TEST_REVIEW_ID_1,
+            TEST_RESTAURANT_ID,
+            5.0 ,
+            TEST_TITLE_1,
+            TEST_BODY_1,
+            TEST_ISO_DATE_TIME_1,
+            null,
+            TEST_USER_METADATA_1
     );
 
     public static final List<Review> TEST_REVIEWS = new ArrayList<>() {
         {
             add(TEST_REVIEW_1);
             add(new Review(
-                TEST_REVIEW_ID_2,
-                TEST_RESTAURANT_ID,
-                7.0 ,
-                TEST_TITLE_2,
-                TEST_BODY_2,
-                TEST_ISO_DATE_TIME_1,
-                TEST_ACCOUNT_ID,
-                TEST_USER_METADATA_1)
+                    TEST_REVIEW_ID_2,
+                    TEST_RESTAURANT_ID,
+                    7.0 ,
+                    TEST_TITLE_2,
+                    TEST_BODY_2,
+                    TEST_ISO_DATE_TIME_1,
+                    TEST_ACCOUNT_ID,
+                    TEST_USER_METADATA_1)
             );
-        }
-    };
-
-    public static final String TEST_DEFAULT_NAME = "test user name";
-    public static final PublicUserMetadataOutput TEST_PUBLIC_USER_METADATA_OUTPUT_WITH_DEFAULT_NAME = new PublicUserMetadataOutput(TEST_DEFAULT_NAME);
-
-    public static final PublicUserMetadataOutput TEST_PUBLIC_USER_METADATA_OUTPUT_EMPTY = new PublicUserMetadataOutput(null);
-
-    public static final List<PublicUserMetadata> TEST_PUBLIC_USER_METADATA_LIST = new ArrayList<>() {
-        {
-            add(new PublicUserMetadata(TEST_ACCOUNT_ID, TEST_USERNAME));
         }
     };
 }
--- src/test/java/com/fryrank/dal/ReviewDALTests.java
+++ src/test/java/com/fryrank/dal/ReviewDALTests.java
@@ -9,11 +9,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.mongodb.core.FindAndReplaceOptions;
 import org.springframework.data.mongodb.core.aggregation.Aggregation;
 import org.springframework.data.mongodb.core.aggregation.AggregationResults;
-import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 import java.util.List;
@@ -21,12 +18,8 @@ import java.util.List;
 import static com.fryrank.TestConstants.TEST_ACCOUNT_ID;
 import static com.fryrank.TestConstants.TEST_RESTAURANT_ID;
 import static com.fryrank.TestConstants.TEST_REVIEWS;
-import static com.fryrank.TestConstants.TEST_REVIEW_1;
-import static com.fryrank.Constants.ISO_DATE_TIME;
 import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
+import static org.junit.Assert.assertThrows;
 import static org.mockito.Mockito.when;
 @RunWith(MockitoJUnitRunner.class)
 public class ReviewDALTests {
@@ -88,23 +81,5 @@ public class ReviewDALTests {
     @Test
     public void testGetAllReviewsByAccountId_nullAccountId() {
         assertThrows(NullPointerException.class, () -> reviewDAL.getAllReviewsByAccountId(null));
-    }
-
-    @Test
-    public void testAddNewReview() throws Exception {
-        when(mongoTemplate.findAndReplace(any(Query.class), eq(TEST_REVIEW_1), any(FindAndReplaceOptions.class))).thenReturn(TEST_REVIEW_1);
-
-        final Review expectedReview = TEST_REVIEW_1;
-        final Review actualReview = reviewDAL.addNewReview(expectedReview);
-        assertEquals(expectedReview, actualReview);
-    }
-
-    @Test public void testGetTopMostRecentReviews() throws Exception {
-        AggregationResults<Review> aggregationResults = new AggregationResults<>(TEST_REVIEWS, new Document());
-        when(mongoTemplate.aggregate(Mockito.any(Aggregation.class), Mockito.anyString(), Mockito.eq(Review.class))).thenReturn(aggregationResults);
-
-        final GetAllReviewsOutput expectedOutput = new GetAllReviewsOutput(TEST_REVIEWS);
-        final GetAllReviewsOutput actualOutput = reviewDAL.getTopMostRecentReviews(TEST_REVIEWS.size());
-        assertEquals(actualOutput, expectedOutput);
     }
 }
